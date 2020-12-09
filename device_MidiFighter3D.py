0787e47cc32621779e5646acf38df0adf7342311
Oladipupo Ogundipe <Oladipupo.Ogundipe@gmail.com>
2020-12-09 16:20:05 -0500

    0.04
    SCALES WORKING, but need to figure out how to turn off the scale :(

--- device_MidiFighter3D.py
+++ device_MidiFighter3D.py
@@ -15,7 +15,7 @@ import playlist
 import math
 import utils
 
-#print(midi.__file__)
+#print(transport.__file__)
 
 class MidiControllerConfig():
     
@@ -24,12 +24,15 @@ class MidiControllerConfig():
         self.shift = 0 #controls the semitons shifted from C3 + Cset
         self.Cset = 12 #controls what octave or how many semitons away from C3 to be the first loew left note on page 1
         self.root = 36 + self.Cset #sets the rote note of the device
+        self.scaleNum = 0 #[Chromatic *0*,Major*1*, minor*2*]
         self.Move = self.Cset + self.shift #tells the program how manne semitones the note should be moved after calculations
         self._shiftUp12_key = [22 + self.Move,28 + self.Move,34 + self.Move,40 + self.Move] #represents notes shifting up an octave
         self._shiftDown12_key = [21 + self.Move,27 + self.Move,33 + self.Move,39 + self.Move] #represets notes that shift down an octave
         self._record_key = [20 + self.Move, 26 + self.Move, 32 + self.Move, 38 + self.Move] #represents notes that controll recording
         self._shiftUp_key = [25 + self.Move,31 + self.Cset,37 + self.Move,43 + self.Move] #represents shifting up one semi
         self._shiftDown_key = [24 + self.Move,30 + self.Move,36 + self.Move,42 + self.Move] #represents shifting down one semi
+        self._harm_key = [23 + self.Move,29 + self.Move,35 + self.Move,41 + self.Move] #changes the key you want 
+    
     def OnInit(self):
         print('init ready')
 
@@ -38,24 +41,52 @@ class MidiControllerConfig():
     
     def OnMidiOutMsg(self, event):
         #print(utils.GetNoteName(event.data1), event.note)
-        #device.midiOutSysex(bytes([0xF0, 0x00, 0x20, 0x6B, 0x7F, 0x42]) + bytes([0xF7]))
-        pass
-
+        #print(device.midiOutMsg(event.midiId, event.midiChan, event.data1, event.data2))
+        
+        
+        if self.scaleNum > 0:
+            #MAJOR-----------------------
+            if self.scaleNum == 1:   
+                i = 36
+                while i < 99:
+                    keyRoot = i % 12
+                    if keyRoot == 0 or keyRoot == 2 or keyRoot == 4 or keyRoot == 5 or keyRoot == 7 or keyRoot == 9 or keyRoot == 11:
+                        device.midiOutMsg(144, 2,  i, i)
+                        device.midiOutMsg(144, 2,  i, i)
+                        event.handled = True
+                    i += 1
+              
+            #MINOR-------------------------
+            if self.scaleNum == 2:   
+                i = 36
+                while i < 99:
+                    keyRoot = i % 12
+                    if keyRoot == 0 or keyRoot == 2 or keyRoot == 3 or keyRoot == 5 or keyRoot == 7 or keyRoot == 8 or keyRoot == 10:
+                        device.midiOutMsg(144, 2,  i, i)
+                        device.midiOutMsg(144, 2,  i, i)
+                        event.handled = True
+                    i += 1   
+        
     def OnMidiMsg(self, event):
         event.handled = False
         event.velocity = 100 #set all notes velocity to fl studios defualt, which is 100 or 78% of 127 rounded up.
 
         #print(event.handled, event.midiId, event.midiChan, event.data1, event.data2)
-        print("midi ID:", event.midiId,"Data: ", event.data1, "Port: ", event.port, "Note: ",event.note, "controlNum: ", event.controlNum,"controlVal: ", event.controlVal,"Event: ", event.outEv)
+        print("midi ID:", event.midiId, "Midi Chan: ", event.midiChan, "Data1: ", event.data1,"Data2: ", event.data1, "Port: ", event.port, "Note: ",event.note, "controlNum: ", event.controlNum,"controlVal: ", event.controlVal,"Event: ", event.outEv)
 
 
 #----------------------------------------------------------Controls Note shifting ----------------------------------------------------------------------#
         self.Move = self.Cset + (self.shift) 
+        
+        #left-side 
         self._shiftUp12_key = [22 + self.Move,28 + self.Move,34 + self.Move,40 + self.Move] 
         self._shiftDown12_key = [21 + self.Move,27 + self.Move,33 + self.Move,39 + self.Move]
         self._record_key = [20 + self.Move, 26 + self.Move, 32 + self.Move, 38 + self.Move]
+
+        #rightside
         self._shiftUp_key = [25 + self.Move,31 + self.Move,37 + self.Move,43 + self.Move]
         self._shiftDown_key = [24 + self.Move,30 + self.Move,36 + self.Move,42 + self.Move]
+        self._harm_key = [23 + self.Move,29 + self.Move,35 + self.Move,41 + self.Move]
 
         
         event.data1 += self.Move #how much each note should be shifted is applied
@@ -63,11 +94,13 @@ class MidiControllerConfig():
        
        
        
-       
-       
-        OnMidiOutMsg(event)
-
-
+#-------------------------------------------Scales Baby--------------------------------------------------------------------------------------------------#       
+        if event.midiId == 0x90 and event.data1 == self._harm_key[self.page]:
+            self.scaleNum += 1
+            self.scaleNum = self.scaleNum % 3
+            OnMidiOutMsg(event)
+            event.handled = True
+#---------------------------------------------------------------------------------------------------------------------------------------------------------#
 
 
 
@@ -140,8 +173,8 @@ class MidiControllerConfig():
 
 
 
-        if event.midiId == 0x90 and event.data1 + self.Move != self._shiftUp12_key[self.page] or event.data1 + self.Move != self._shiftDown12_key[self.page] or event.data1 + self.Move != self._record_key[self.page]:
-            print(utils.GetNoteName(event.data1), event.note)
+        #if event.midiId == 0x90 and event.data1 + self.Move != self._shiftUp12_key[self.page] or event.data1 + self.Move != self._shiftDown12_key[self.page] or event.data1 + self.Move != self._record_key[self.page]:
+            #print(utils.GetNoteName(event.data1), event.note)
             
             
 
